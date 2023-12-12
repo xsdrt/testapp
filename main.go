c46cafe83377079c215c7ded973d7d8b4105eefe
SandmanR <mredinger4@gmail.com>
2023-12-11 21:33:10 -0500

    just cleaned up the app funcs

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/xsdrt/hispeed2"
-)
-
 func main() {
-	result := hispeed2.TestFunc(1, 1)
-	fmt.Println(result)
-
-	result = hispeed2.TestFunc2(2, 1)
-	fmt.Println(result)
-
-	result = hispeed2.TestFunc3(2, 2)
-	fmt.Println(result)
 
 }
