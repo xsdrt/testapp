c1a4810e1ef3d7b802f4ba2684c6dc0e0bde4679
SandmanR <mredinger4@gmail.com>
2024-02-15 21:34:34 -0500

    Making sure my HiSpeed2 framework sessions is working with my testApp:it does...

--- vendor/github.com/xsdrt/hispeed2/middleware.go
+++ vendor/github.com/xsdrt/hispeed2/middleware.go
@@ -3,5 +3,6 @@ package hispeed2
 import "net/http"
 
 func (h *HiSpeed2) SessionLoad(next http.Handler) http.Handler {
+	h.InfoLog.Println("SessionLoad called")
 	return h.Session.LoadAndSave(next)
 }
