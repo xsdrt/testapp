5a84e322ad3001e0fc45b5313bb40a72d4cc4c2b
SandmanR <mredinger4@gmail.com>
2024-02-29 17:35:32 -0500

    Continuing dev of ORM upper/db in the test app with more functions for the user type....

--- data/models.go
+++ data/models.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 
 	db2 "github.com/upper/db/v4"
@@ -28,4 +29,13 @@ func New(databasePool *sql.DB) Models {
 	}
 
 	return Models{}
+}
+
+func getInsertID(i db2.ID) int {
+	idType := fmt.Sprintf("%T", i)
+	if idType == "int64" { // If int64, then postgres; return i cast to a int64...
+		return int(i.(int64))
+	}
+
+	return i.(int) // Otherwise just return i cast to an int...
 }
--- data/user.go
+++ data/user.go
@@ -3,6 +3,8 @@ package data
 import (
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
+
 	up "github.com/upper/db/v4"
 )
 
@@ -22,12 +24,12 @@ func (u *User) Table() string { //this func gives us the ability to overide the 
 	return "users" // so anytime refering to the table users in the db can overide; so,if legacy is called customers, overide and will refer to the users in the DB...
 }
 
-func (u *User) GetAll(condition up.Cond) ([]*User, error) {
+func (u *User) GetAll() ([]*User, error) {
 	collection := upper.Collection(u.Table()) // FYI upper refers to data stored in the DB as collections so will use their naming convetion...
 
 	var all []*User
 
-	res := collection.Find(condition)
+	res := collection.Find().OrderBy("last_name")
 	err := res.All((&all))
 	if err != nil {
 		return nil, err
@@ -58,4 +60,92 @@ func (u *User) GetByEmail(email string) (*User, error) {
 	theUser.Token = token
 
 	return &theUser, nil
+}
+
+func (u *User) Get(id int) (*User, error) {
+	var theUser User
+	collection := upper.Collection(u.Table())
+	res := collection.Find(up.Cond{"id =": id})
+
+	err := res.One(&theUser)
+	if err != nil {
+		return nil, err
+	}
+
+	var token Token
+	collection = upper.Collection(token.Table())
+	res = collection.Find(up.Cond{"user_id =": theUser.ID, "expiry <": time.Now()}).OrderBy("created_at desc")
+	err = res.One(&token)
+	if err != nil {
+		if err != up.ErrNilRecord && err != up.ErrNoMoreRows {
+			return nil, err
+		}
+	}
+
+	theUser.Token = token
+
+	return &theUser, nil
+}
+
+func (u *User) Update(theUser User) error {
+	theUser.UpdatedAt = time.Now()
+	collection := upper.Collection(u.Table())
+	res := collection.Find(theUser.ID)
+	err := res.Update(&theUser)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *User) Delete(id int) error {
+	collection := upper.Collection(u.Table())
+	res := collection.Find(id)
+	err := res.Delete()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *User) Insert(theUser User) (int, error) {
+	newHash, err := bcrypt.GenerateFromPassword([]byte(theUser.Password), 12)
+	if err != nil {
+		return 0, err
+	}
+
+	theUser.CreatedAt = time.Now()
+	theUser.UpdatedAt = time.Now()
+	theUser.Password = string(newHash)
+
+	collection := upper.Collection(u.Table())
+	res, err := collection.Insert(theUser)
+	if err != nil {
+		return 0, err
+	}
+
+	id := getInsertID(res.ID())
+
+	return id, nil
+}
+
+func (u *User) ResetPassword(id int, password string) error {
+	newHash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	theUser, err := u.Get(id)
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(newHash)
+
+	err = theUser.Update(*u)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
