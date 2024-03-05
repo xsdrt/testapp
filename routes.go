b88342f354b494e7c6e63d22e2f2ff07213c3528
SandmanR <mredinger4@gmail.com>
2024-03-05 11:55:46 -0500

    Added some inline funcs to get all users, get user by id, update a user with a random name using the RandomString func...

--- routes.go
+++ routes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"testapp/data"
 
 	"github.com/go-chi/chi/v5"
@@ -33,6 +34,46 @@ func (a *application) routes() *chi.Mux {
 		}
 
 		fmt.Fprintf(w, "%d: %s", id, u.FirstName)
+	})
+
+	a.App.Routes.Get("/get-all-users", func(w http.ResponseWriter, r *http.Request) { // inline test func...
+		users, err := a.Models.Users.GetAll()
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		for _, x := range users {
+			fmt.Fprint(w, x.LastName)
+		}
+	})
+
+	a.App.Routes.Get("/get-user/{id}", func(w http.ResponseWriter, r *http.Request) { // inline test func...
+		id, _ := strconv.Atoi(chi.URLParam(r, "id")) // gives the id thats in the url...
+
+		u, err := a.Models.Users.Get(id)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+		fmt.Fprintf(w, "%s %s %s", u.FirstName, u.LastName, u.Email)
+	})
+
+	a.App.Routes.Get("/update-user/{id}", func(w http.ResponseWriter, r *http.Request) { // inline test func...
+		id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+		u, err := a.Models.Users.Get(id)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+
+		u.LastName = a.App.RandomString(10)
+		err = u.Update(*u)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+
+		fmt.Fprintf(w, "updated last name to %s", u.LastName)
 	})
 
 	// static routes
--- vendor/github.com/xsdrt/hispeed2/helpers.go
+++ vendor/github.com/xsdrt/hispeed2/helpers.go
@@ -1,6 +1,25 @@
 package hispeed2
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321_+"
+)
+
+// RandomString generates a random string of length n from values in the const randomString...
+func (h *HiSpeed2) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
 
 func (h *HiSpeed2) CreateDirIfNotExist(path string) error {
 	const mode = 0755
