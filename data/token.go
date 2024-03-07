13a6918de23171cfe51cefd3dd0f2850ac782148
SandmanR <mredinger4@gmail.com>
2024-03-06 21:11:05 -0500

    Starting to add function(s) to the Token Model...

--- data/token.go
+++ data/token.go
@@ -1,6 +1,16 @@
 package data
 
-import "time"
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base32"
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+
+	up "github.com/upper/db/v4"
+)
 
 type Token struct {
 	ID        int       `db:"id" json:"id"`
@@ -14,6 +24,173 @@ type Token struct {
 	Expires   time.Time `db:"expiry" json:"expiry"`
 }
 
+// Below are all the database functions for tokens...
+
 func (t *Token) Table() string {
 	return "tokens"
+}
+
+// Allows us to get a user by token...
+func (t *Token) GetUserForToken(token string) (*User, error) {
+	var u User
+	var theToken Token
+
+	collection := upper.Collection(t.Table())
+	res := collection.Find(up.Cond{"token": token})
+	err := res.One(&theToken)
+	if err != nil {
+		return nil, err
+	}
+
+	collection = upper.Collection("user")
+	res = collection.Find(up.Cond{"id": theToken.UserID})
+	err = res.One(&u)
+	if err != nil {
+		return nil, err
+	}
+
+	u.Token = theToken
+
+	return &u, nil
+}
+
+// get all the tokens for a given user...
+func (t *Token) GetTokensForUser(id int) ([]*Token, error) {
+	var tokens []*Token
+	collection := upper.Collection(t.Table())
+	res := collection.Find(up.Cond{"user_id": id})
+	err := res.All(&tokens)
+	if err != nil {
+		return nil, err
+	}
+	return tokens, nil
+}
+
+// Get a token by id...
+func (t *Token) Get(id int) (*Token, error) {
+	var token Token
+	collection := upper.Collection(t.Table())
+	res := collection.Find(up.Cond{"id": id})
+	err := res.One(&token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &token, nil
+}
+
+// Get token by plainText...
+func (t *Token) GetByToken(plainText string) (*Token, error) {
+	var token Token
+	collection := upper.Collection(t.Table())
+	res := collection.Find(up.Cond{"token": plainText})
+	err := res.One(&token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &token, nil
+}
+
+// Delete a token by id, especially when a user logs out...
+func (t *Token) Delete(id int) error {
+	collection := upper.Collection(t.Table())
+	res := collection.Find(id)
+	err := res.Delete()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete by token(plainText)...
+func (t *Token) DeleteByToken(plainText string) error {
+	collection := upper.Collection(t.Table())
+	res := collection.Find(up.Cond{"token": plainText})
+	err := res.Delete()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Insert a token into the database...
+func (t *Token) Insert(token Token, u User) error {
+	collection := upper.Collection(t.Table())
+
+	// Delete existing tokens if inserting a new token(s)...
+	res := collection.Find(up.Cond{"user_id": u.ID})
+	err := res.Delete()
+	if err != nil {
+		return err
+	}
+
+	token.CreatedAt = time.Now()
+	token.UpdatedAt = time.Now()
+	token.FirstName = u.FirstName
+	token.Email = u.Email
+
+	_, err = collection.Insert(token)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Need to generate a token before they can be inserterd of course...
+func (t *Token) GenerateToken(userID int, ttl time.Duration) (*Token, error) {
+	token := &Token{
+		UserID:  userID,
+		Expires: time.Now().Add(ttl),
+	}
+
+	randomBytes := make([]byte, 16)
+	_, err := rand.Read(randomBytes) // Using crypto/rand pkg...
+	if err != nil {
+		return nil, err
+	}
+
+	token.PlainText = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
+	hash := sha256.Sum256([]byte(token.PlainText))
+	token.Hash = hash[:]
+
+	return token, nil
+}
+
+// Authenticate the token....
+func (t *Token) AuthenticateToken(r *http.Request) (*User, error) {
+	authorizationHeader := r.Header.Get("Authorization")
+	if authorizationHeader == "" {
+		return nil, errors.New("no authorization header received")
+	}
+
+	headerParts := strings.Split(authorizationHeader, " ")
+	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+		return nil, errors.New("no authorization header received")
+	}
+
+	token := headerParts[1]
+
+	if len(token) != 26 { // every token should be 26 characters long...
+		return nil, errors.New("token wrong size")
+	}
+
+	t, err := t.GetByToken(token)
+	if err != nil {
+		return nil, errors.New("no matching token found")
+	}
+
+	if t.Expires.Before(time.Now()) {
+		return nil, errors.New("expired token")
+	}
+
+	user, err := t.GetUserForToken(token)
+	if err != nil {
+		return nil, errors.New("no matching user found")
+	}
+
+	return user, nil
 }
