9afc4068a5f6fdf8f86fb5bb73f1203180311e7e
xsdrt <m_redinger@hotmail.com>
2024-04-18 21:46:49 -0400

    Dev handlers for the new response types in HiSpeed2...

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,7 +14,7 @@ type Handlers struct {
 }
 
 func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
-	err := h.render(w, r, "home", nil, nil)		// using the handler helper func from the convience.go file in Handlers...
+	err := h.render(w, r, "home", nil, nil) // using the handler helper func from the convience.go file in Handlers...
 	if err != nil {
 		h.App.ErrorLog.Println("error renderering:", err)
 	}
@@ -48,4 +48,43 @@ func (h *Handlers) SessionTest(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		h.App.ErrorLog.Println("error rendering:", err)
 	}
+}
+
+func (h *Handlers) JSON(w http.ResponseWriter, r *http.Request) {
+	var payload struct {
+		ID      int64    `json:"id"`
+		Name    string   `json:"name"`
+		Hobbies []string `json:"hobbies"`
+	}
+
+	payload.ID = 10
+	payload.Name = "Pete sake"
+	payload.Hobbies = []string{"Blueberry Picking", "Pickle Ball", "Scrabbel"}
+
+	err := h.App.WriteJSON(w, http.StatusOK, payload)
+	if err != nil {
+		h.App.ErrorLog.Println(err)
+	}
+}
+
+func (h *Handlers) XML(w http.ResponseWriter, r *http.Request) {
+	type Payload struct {
+		ID      int64    `xml:"id"`
+		Name    string   `xml:"name"`
+		Hobbies []string `xml:"hobbies>hobby"`
+	}
+
+	var payload Payload
+	payload.ID = 10
+	payload.Name = "Pete Sake"
+	payload.Hobbies = []string{"Blueberry Picking", "Pickle Ball", "Scrabbel"}
+
+	err := h.App.WriteXML(w, http.StatusOK, payload)
+	if err != nil {
+		h.App.ErrorLog.Println(err)
+	}
+}
+
+func (h *Handlers) DownloadFile(w http.ResponseWriter, r *http.Request) {
+	h.App.DownloadFile(w, r, "./public/images", "hsld.jpg")
 }
--- vendor/github.com/xsdrt/hispeed2/response-utils.go
+++ vendor/github.com/xsdrt/hispeed2/response-utils.go
@@ -2,7 +2,11 @@ package hispeed2
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"fmt"
 	"net/http"
+	"path"
+	"path/filepath"
 )
 
 func (h *HiSpeed2) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
@@ -24,4 +28,53 @@ func (h *HiSpeed2) WriteJSON(w http.ResponseWriter, status int, data interface{}
 		return err
 	}
 	return nil
+}
+
+func (h *HiSpeed2) WriteXML(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := xml.MarshalIndent(data, "", "	")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *HiSpeed2) DownloadFile(w http.ResponseWriter, r *http.Request, pathToFile, fileName string) error {
+	fp := path.Join(pathToFile, fileName)
+	fileToServe := filepath.Clean(fp)
+	w.Header().Set("Content-Type", fmt.Sprintf("attachment; file=\"%s\"", fileName))
+	http.ServeFile(w, r, fileToServe)
+	return nil
+}
+
+func (h *HiSpeed2) Error404(w http.ResponseWriter, r *http.Request) {
+	h.ErrorStatus(w, http.StatusNotFound)
+}
+
+func (h *HiSpeed2) Error500(w http.ResponseWriter, r *http.Request) {
+	h.ErrorStatus(w, http.StatusInternalServerError)
+}
+
+func (h *HiSpeed2) ErrorUnauthorized(w http.ResponseWriter, r *http.Request) {
+	h.ErrorStatus(w, http.StatusUnauthorized)
+}
+
+func (h *HiSpeed2) ErrorForbidden(w http.ResponseWriter, r *http.Request) {
+	h.ErrorStatus(w, http.StatusForbidden)
+}
+
+func (h *HiSpeed2) ErrorStatus(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
 }
